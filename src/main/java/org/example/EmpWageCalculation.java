324725eccc7b5ef9d5e84473771219016131c0b5
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-04 00:19:02 +0530

    Added: Employee daily wage calculated

--- src/main/java/org/example/EmpWageCalculation.java
+++ src/main/java/org/example/EmpWageCalculation.java
@@ -2,17 +2,20 @@ package org.example;
 
 public class EmpWageCalculation
 {
-    static int EMP_PRESENT=1;
-    static int empAttendance;
+    static int EMP_PRESENT=1,WAGE_PER_HR=20,FULL_DAY=8;
+    static int empAttendance,salary;
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Calculation!" );
         empAttendance = (int) Math.floor(Math.random() * 10) % 2;
         if(empAttendance==EMP_PRESENT){
             System.out.println("Employee is present");
+            salary=WAGE_PER_HR*FULL_DAY;
         } else{
             System.out.println("Employee is absent");
+            salary=0;
         }
+        System.out.println("Employee's Wage= "+salary);
     }
 
 
