5e03282a465d4defe41725da2a032c7b4c9cbc77
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-04 00:10:46 +0530

    Added: Employee Attendance checked

--- src/main/java/org/example/EmpWageCalculation.java
+++ src/main/java/org/example/EmpWageCalculation.java
@@ -1,13 +1,20 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
 public class EmpWageCalculation
 {
+    static int EMP_PRESENT=1;
+    static int empAttendance;
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Calculation!" );
+        empAttendance = (int) Math.floor(Math.random() * 10) % 2;
+        if(empAttendance==EMP_PRESENT){
+            System.out.println("Employee is present");
+        } else{
+            System.out.println("Employee is absent");
+        }
     }
+
+
 }
+
