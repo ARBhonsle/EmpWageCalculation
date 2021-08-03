191eff38fcd664f8870d8578a9a2292c63fbc3ff
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-04 00:25:44 +0530

    Added: Employee part-time wage calculated

--- src/main/java/org/example/EmpWageCalculation.java
+++ src/main/java/org/example/EmpWageCalculation.java
@@ -2,16 +2,20 @@ package org.example;
 
 public class EmpWageCalculation
 {
-    static int EMP_PRESENT=1,WAGE_PER_HR=20,FULL_DAY=8;
+    static int EMP_PRESENT=1,WAGE_PER_HR=20,FULL_DAY=8,PART_TIME=2,PART_TIME_HR=4;
     static int empAttendance,salary;
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Calculation!" );
-        empAttendance = (int) Math.floor(Math.random() * 10) % 2;
+        empAttendance = (int) Math.floor(Math.random() * 10) % 3;
         if(empAttendance==EMP_PRESENT){
             System.out.println("Employee is present");
             salary=WAGE_PER_HR*FULL_DAY;
-        } else{
+        } else if(empAttendance==PART_TIME){
+            System.out.println("Employee is present part time");
+            salary=WAGE_PER_HR*PART_TIME_HR;
+        }
+        else{
             System.out.println("Employee is absent");
             salary=0;
         }
