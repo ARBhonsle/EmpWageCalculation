be424a5dbccab9ce7e137e64e0ace20b3a76d2ad
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-04 17:01:34 +0530

    Modified: Monthly wage calculated by repeating for total working days per month

--- src/main/java/org/example/EmpWageCalculation.java
+++ src/main/java/org/example/EmpWageCalculation.java
@@ -4,35 +4,40 @@ package org.example;
  * EmpWageCalculation computes wage of employee
  * checks attendance using random and evaluates wage by
  * multiplying Hrs worked(from attendance) and WAGE_PER_HR
+ * and monthly wage by repeating for working days of a month
  */
 public class EmpWageCalculation
 {
     // constants
     final static int EMP_PRESENT=1,PART_TIME=2;
     final static int WAGE_PER_HR=20,FULL_DAY=8,PART_TIME_HR=4,ABSENTEE_WAGE=0;
+    final static int WORKING_DAYS=20;
     //variables
-    static int empAttendance,salary;
+    static int empAttendance,salary,monthlySalary,day;
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Calculation!" );
-        empAttendance = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empAttendance){
-            case EMP_PRESENT:
-                System.out.println("Employee is present");
-                salary=WAGE_PER_HR*FULL_DAY;
-                break;
-            case PART_TIME:
-                System.out.println("Employee is present part time");
-                salary=WAGE_PER_HR*PART_TIME_HR;
-                break;
-            default:
-                System.out.println("Employee is absent");
-                salary=ABSENTEE_WAGE;
-                break;
+        monthlySalary=0;
+        for(day=0;day<WORKING_DAYS;day++){
+            empAttendance = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empAttendance){
+                case EMP_PRESENT:
+                    System.out.println("Employee is present");
+                    salary=WAGE_PER_HR*FULL_DAY;
+                    break;
+                case PART_TIME:
+                    System.out.println("Employee is present part time");
+                    salary=WAGE_PER_HR*PART_TIME_HR;
+                    break;
+                default:
+                    System.out.println("Employee is absent");
+                    salary=ABSENTEE_WAGE;
+                    break;
+            }
+            monthlySalary+=salary;
+            System.out.println("Wage: "+salary);
         }
-        System.out.println("Employee's Wage= "+salary);
+        System.out.println("Monthly Wage: "+monthlySalary);
     }
-
-
 }
 
