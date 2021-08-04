cfa2f3973bb019014c01820c99b63d68be031d26
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-08-04 17:11:42 +0530

    Modified: Employee wage calculated until total working hrs or days reached for a month

--- src/main/java/org/example/EmpWageCalculation.java
+++ src/main/java/org/example/EmpWageCalculation.java
@@ -11,26 +11,31 @@ public class EmpWageCalculation
     // constants
     final static int EMP_PRESENT=1,PART_TIME=2;
     final static int WAGE_PER_HR=20,FULL_DAY=8,PART_TIME_HR=4,ABSENTEE_WAGE=0;
-    final static int WORKING_DAYS=20;
+    final static int WORKING_DAYS=20,WORKING_HRS_TOTAL=100;
     //variables
-    static int empAttendance,salary,monthlySalary,day;
+    static int empAttendance,salary,monthlySalary,day,hrs;
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Calculation!" );
-        monthlySalary=0;
-        for(day=0;day<WORKING_DAYS;day++){
+        monthlySalary=0;hrs=0;
+        for(day=0;day<WORKING_DAYS && hrs<WORKING_HRS_TOTAL;day++){
             empAttendance = (int) Math.floor(Math.random() * 10) % 3;
             switch (empAttendance){
                 case EMP_PRESENT:
                     System.out.println("Employee is present");
                     salary=WAGE_PER_HR*FULL_DAY;
+                    System.out.println("Hours: "+FULL_DAY);
+                    hrs+=FULL_DAY;
                     break;
                 case PART_TIME:
                     System.out.println("Employee is present part time");
                     salary=WAGE_PER_HR*PART_TIME_HR;
+                    System.out.println("Hours: "+PART_TIME_HR);
+                    hrs+=PART_TIME_HR;
                     break;
                 default:
                     System.out.println("Employee is absent");
+                    System.out.println("Hours: "+0);
                     salary=ABSENTEE_WAGE;
                     break;
             }
@@ -38,6 +43,7 @@ public class EmpWageCalculation
             System.out.println("Wage: "+salary);
         }
         System.out.println("Monthly Wage: "+monthlySalary);
+        System.out.println("Working hrs: "+hrs);
     }
 }
 
